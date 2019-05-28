426dd6e534f47c1d29d51643c13d5315eeaa5e52
ramir7887 <ramir7887@yandex.ru>
2019-05-28 18:38:32 +0300

    start webpanel project

---
+++ servertornado.py
@@ -0,0 +1,53 @@
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+import socket
+import json
+'''
+This is a simple Websocket Echo server that uses the Tornado websocket handler.
+Please run `pip install tornado` with python of version 2.7.9 or greater to install tornado.
+This program will echo back the reverse of whatever it recieves.
+Messages are output to the terminal for debuggin purposes. 
+''' 
+import linuxcnc
+c =linuxcnc.command()
+c.mode(linuxcnc.MODE_MANUAL)
+
+ 
+class WSHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print ('new connection')
+      
+    def on_message(self, message):
+        print 'message received:  %s' % message
+        # Reverse Message and send it back
+        print(message, type(message))
+        js = json.loads(message)
+        mes = dict(js)
+        if mes['action']=='btY':
+            print 'Get BTY'
+            c.jog(linuxcnc.JOG_CONTINUOUS, 1, 100)
+        if mes['action']=='btS':
+            print 'Get BTY'
+            c.jog(linuxcnc.JOG_STOP, 1)
+
+ 
+    def on_close(self):
+        print ('connection closed')
+ 
+    def check_origin(self, origin):
+        return True
+ 
+application = tornado.web.Application([
+    (r'/ws', WSHandler),
+])
+print(application.default_host)
+ 
+ 
+if __name__ == "__main__":
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8889, address= '192.168.100.245')
+    myIP = socket.gethostbyname(socket.gethostname())
+    print '*** Websocket Server Started at %s***' % myIP
+    tornado.ioloop.IOLoop.instance().start()
